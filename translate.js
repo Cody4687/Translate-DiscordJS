4a216a0222ef9c1efcc2f1ea06aae3dd0b3f21dc
Cody <38409768+Cody4687@users.noreply.github.com>
2018-10-15 22:10:40 -0500

    Update translate.js

--- translate.js
+++ translate.js
@@ -7,7 +7,7 @@ var translate = require('node-google-translate-skidz');
 
 bot.on("ready", () => {
     console.log('bot is online!');
-    bot.user.setActivity("with thousands of minecraft accounts.", {type: "PLAYING"});
+    bot.user.setActivity("-help", {type: "PLAYING"});
 });
 
 bot.on("message", message => {
@@ -18,6 +18,44 @@ bot.on("message", message => {
   var cmd = messageArray[0];
   var args = messageArray.slice(1);
   var translateee = args.join(" ");
+
+  if (cmd === `${prefix}help`)
+  return message.channel.send({embed: {
+        color: 0xffffff,
+        title: "Help:",
+        description: "This is a bot made by Cody#5507 that is used to translate any language to any language, do -codes for the language list and -usage to learn how to use the bot.",
+        timestamp: new Date(),
+        footer: {
+          text: message.author.tag
+        }
+      }
+    });
+
+    if (cmd === `${prefix}usage`)
+    return message.channel.send({embed: {
+          color: 0xffffff,
+          title: "Usage:",
+          description: "To use this bot, you need to know language codes, if you do not know them do -codes. Then you can use the bot, to translate use code1-code2, an example is (en-es Hello my name is cody) to translate from english to spanish.",
+          timestamp: new Date(),
+          footer: {
+            text: message.author.tag
+          
+          }
+        }
+      });
+
+      if (cmd === `${prefix}codes`)
+      return message.channel.send({embed: {
+            color: 0xffffff,
+            title: "Codes:",
+            description: "Amharic	am,            Arabic	ar,            Basque	eu,            Bengali	bn,            English (UK)	en-GB,            Portuguese (Brazil)	pt-BR,            Bulgarian	bg,            Catalan	ca,            Cherokee	chr,            Croatian	hr            Czech	cs,            Danish	da,            Dutch	nl,            English (US)	en,            Estonian	et,            Filipino	fil,            Finnish	fi,            French	fr,            German	de,,            Greek	el,            Gujarati	gu,            Hebrew	iw,            Hindi	hi,            Hungarian	hu,            Icelandic	is,            Indonesian	id,            Italian	it,            Japanese	ja,            Kannada	kn,            Korean	ko,            Latvian	lv,            Lithuanian	lt,            Malay	ms,            Malayalam	ml,            Marathi	mr,            Norwegian	no,            Polish	pl,            Portuguese (Portugal)	pt-PT,            Romanian	ro,            Russian	ru,            Serbian	sr,            Chinese (PRC)	zh-CN,            Slovak	sk,            Slovenian	sl,            Spanish	es,            Swahili	sw,            Swedish	sv,            Tamil	ta,            Telugu	te,            Thai	th,            Chinese (Taiwan)	zh-TW,            Turkish	tr,            Urdu	ur,            Ukrainian	uk,            Vietnamese	vi,            Welsh	cy,",
+            timestamp: new Date(),
+            footer: {
+              text: message.author.tag
+            }
+          }
+        });
+  
 
   try {
   {
